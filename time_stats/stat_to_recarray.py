7af6d547d2ce7c287d0bce471b13c2020a045fd5
Vlad Popa <vlad.popa@gmail.com>
2014-10-16 13:37:12 -0700

    Convert plume stats to recarray; convert core/condensed/plume on every run

--- time_stats/stat_to_recarray.py
+++ time_stats/stat_to_recarray.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-
-import numpy
+import numpy as np
 import cPickle
 
-# pklfile = cPickle.load(open('pkl/core_time_stats.pkl', 'rb'))
-pklfile = cPickle.load(open('pkl/condensed_time_stats.pkl', 'rb'))
-
+pklfile = cPickle.load(open('pkl/core_time_stats.pkl', 'rb'))
 k = len(pklfile.keys())
 n = len(pklfile['z'])
-
-stats = numpy.zeros(1, dtype = [(key, 'f8', n) for key in pklfile])
-
+stats = np.zeros(1, dtype = [(key, 'f8', n) for key in pklfile])
 for key in pklfile:
    stats[key] = pklfile[key]
+np.save('npy/core_time_stats.npy', stats)
 
-# numpy.save('npy/core_time_stats.npy', stats)
-numpy.save('npy/condensed_time_stats.npy', stats)
+pklfile = cPickle.load(open('pkl/condensed_time_stats.pkl', 'rb'))
+k = len(pklfile.keys())
+n = len(pklfile['z'])
+stats = np.zeros(1, dtype = [(key, 'f8', n) for key in pklfile])
+for key in pklfile:
+   stats[key] = pklfile[key]
+np.save('npy/condensed_time_stats.npy', stats)
+
+pklfile = cPickle.load(open('pkl/plume_time_stats.pkl', 'rb'))
+k = len(pklfile.keys())
+n = len(pklfile['z'])
+stats = np.zeros(1, dtype = [(key, 'f8', n) for key in pklfile])
+for key in pklfile:
+   stats[key] = pklfile[key]
+np.save('npy/plume_time_stats.npy', stats)
