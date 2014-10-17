7473efc6d11139efd0c5e7975677db02de7ead12
Vlad Popa <vlad.popa@gmail.com>
2014-10-16 17:44:12 -0700

    Plume surface profiles added

--- time_profiles/surface_profiles.py
+++ time_profiles/surface_profiles.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-#Runtime (690, 130, 128, 128): 3 hours 40 minutes
-
 from pylab import *
-import numpy
+import numpy as np
 import cPickle
 from netCDF4 import Dataset
 import os
 from ent_analysis.lib.thermo import SAM
 import ent_analysis.lib.model_param as mc
 
-# Load mean cloud field stat
+# Load mean cloud field statistics
 stat_file = Dataset(mc.get_stat())
 data = {'z': stat_file.variables['z'][:].astype(double),
     'RHO' : stat_file.variables['RHO'][0,:].astype(double),
@@ -17,6 +15,11 @@ data = {'z': stat_file.variables['z'][:].astype(double),
 stat_file.close()
 
 def create_savefile(t, data, vars, profile_name):
+    """ Create netCDF file for specified profile at the current time step.
+    netCDF axes are id and z.
+    
+    Return: netCDF dataset and variables
+    """
     ids = data['ids'][:]
     z = data['z'][:]
     savefile = Dataset('cdf/%s_profile_%08d.nc' % (profile_name, t), 'w')
@@ -36,10 +39,11 @@ def create_savefile(t, data, vars, profile_name):
         
     return savefile, variables
 
-#--------------
-
 def make_profiles(variables, cloud_data, vars, data, n):
-    temp = {'core': 'CORE_SURFACE', 'condensed': 'CONDENSED_SURFACE'}
+    """Make profiles for a single cloud at the current time step.
+    """
+    temp = {'core': 'CORE_SURFACE', 'condensed': 'CONDENSED_SURFACE', 
+        'plume': 'PLUME_SURFACE'}
     
     for item in temp:                
         temp_profile = {}
@@ -49,29 +53,29 @@ def make_profiles(variables, cloud_data, vars, data, n):
         indexes = cloud_data[item]
         if len(indexes) > 0:
             indexes2 = indexes + mc.nx*mc.ny
-            a = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            a = ~np.in1d(indexes, indexes2, assume_unique=True)
             indexes2 = indexes - mc.nx*mc.ny
-            b = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            b = ~np.in1d(indexes, indexes2, assume_unique=True)
         
             z, y, x = mc.index_to_zyx(indexes)
         
             x2  = (x+1) % mc.nx
             indexes2 = mc.zyx_to_index(z, y, x2)
-            c = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            c = ~np.in1d(indexes, indexes2, assume_unique=True)
             x2  = (x-1) % mc.nx
             indexes2 = mc.zyx_to_index(z, y, x2)
-            d = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            d = ~np.in1d(indexes, indexes2, assume_unique=True)
 
             y2  = (y+1) % mc.ny
             indexes2 = mc.zyx_to_index(z, y2, x)
-            e = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            e = ~np.in1d(indexes, indexes2, assume_unique=True)
             y2  = (y-1) % mc.ny
             indexes2 = mc.zyx_to_index(z, y2, x)
-            f = ~numpy.in1d(indexes, indexes2, assume_unique=True)
+            f = ~np.in1d(indexes, indexes2, assume_unique=True)
             
             area = mc.dx*mc.dy*(a+b) + mc.dy*mc.dz*(c+d) + mc.dx*mc.dz*(e+f)
 
-            for k in numpy.unique(z):
+            for k in np.unique(z):
                 mask = z == k
                 temp_profile[temp[item]][k] = area[mask].sum()
             
@@ -79,23 +83,19 @@ def make_profiles(variables, cloud_data, vars, data, n):
                                                
         variables[temp[item]][n, :] = results[temp[item]]
 
-
-#------------------
-
 def main(t):
-	vars = ('CORE_SURFACE', 'CONDENSED_SURFACE')
+	vars = ('CORE_SURFACE', 'CONDENSED_SURFACE', 'PLUME_SURFACE')
 
-	# For each cloud, iterate over all times
+	# Load cloud data for current time step
 	cloud_filename = '../cloudtracker/pkl/cloud_data_%08d.pkl' % t
-	# Load the cloud data at that timestep
 	clouds = cPickle.load(open(cloud_filename, 'rb'))
 
 	ids = clouds.keys()
 	ids.sort()
 
-	data['ids'] = numpy.array(ids)
+	data['ids'] = np.array(ids)
 
-	# For each cloud, create a savefile for each profile
+    # Create a savefile for surface profile at the current time step
 	savefile, variables = create_savefile(t, data, vars, 'surface')
 
 	for n, id in enumerate(ids):
